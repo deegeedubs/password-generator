4e89de592737b47b9a717a1ac6b5f132503f8cd7
Danny Wittig <wittigdaniel.g@gmail.com>
2023-04-15 10:47:34 -0500

    Added pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -6,8 +6,25 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// Ask which criteria to include
 
+// Generate password of x length
+
+// Multiple if statements to run different function checks based on what criteria is needed
+
+// Function to check capital letters 
+
+// Function to check lowercase letters
+
+// Function to check numbers
+
+// Function to check special characters
+
+// If any of these do not pass, generate a new password and check again
+
+
+// This will output the value of password to HTML:
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
